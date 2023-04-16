ea45236324e85dd455ce3be7adcee3ca33020828
Monika1924 <monika2005md@gmail.com>
2023-04-16 06:10:40 +0530

    change homebutton

--- resources/views/Template/home.blade.php
+++ resources/views/Template/home.blade.php
@@ -1,8 +1,7 @@
 @include('Template.script')
 @include('Template.link')
-<ul class="navbar-nav ">
-    <li class="nav-item p-3">
-        <button type="button" class="btn btn-outline-success border-2 justify-content-end"><span class="h6"><a
+
+<div class="text-center mb-4">
+        <button type="button" class="btn btn-outline-success border-2"><span class="h6"><a
                     href="{{ url('/') }}" class="nav-link"> Back to Home</a></span></button>
-    </li>
-</ul>
+</div>
